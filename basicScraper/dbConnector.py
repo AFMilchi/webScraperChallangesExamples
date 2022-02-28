82cbcdc113215c234558d291134d0952a8262baf
Milchbengel <81463477+Milchbengel@users.noreply.github.com>
2022-02-28 12:10:04 +0100

    Delete basicScraper directory

--- basicScraper/dbConnector.py
+++
@@ -1,8 +0,0 @@
-class DbConnector:
-    def __init__(self):
-        pass
-
-    def writeOut(self, data):
-        with open('./moduleList.txt', 'w') as file:
-            for element in data:
-                file.writelines(str(element)+'\n')
